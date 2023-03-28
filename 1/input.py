efad60e104270d7ae7837826abe5e5f11cdcb7a1
Emmanuel <70802541+MenuItem207@users.noreply.github.com>
2023-03-28 14:50:18 +0800

    Update input.py

--- 1/input.py
+++ 1/input.py
@@ -3,5 +3,10 @@ str_value = input("Who are you?: ")
 print(str_value)
 
 # * Excercise
-# prompt the user for their name and birth year and print out the following
+# 1. prompt the user for their name and birth year and print out the following
 # Welcome _name_, you are _age_ this year!
+
+# 2. write a calculator program that prompts for 2 values i.e
+# value_one = input('please enter your first value')
+# value_two = input('please enter your second value')
+# and print the sum, difference (subtract), product (multiply), division and remainder of the two
