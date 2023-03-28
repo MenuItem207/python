fc28941854ab918d3e04c97203e13a6a45254fc4
Emmanuel <70802541+MenuItem207@users.noreply.github.com>
2023-03-28 14:46:47 +0800

    Create input.py

---
+++ 1/input.py
@@ -0,0 +1,7 @@
+# input can be used to prompt the user for a string
+str_value = input("Who are you?: ")
+print(str_value)
+
+# * Excercise
+# prompt the user for their name and birth year and print out the following
+# Welcome _name_, you are _age_ this year!
